c49ad2a22ddfd3ee25a93464b69072b2d82eaa10
maehachi08 <maehachi08@gmail.com>
2020-02-16 00:41:01 +0900

    test: add unittest

--- tests/libs/test_images.py
+++ tests/libs/test_images.py
@@ -1,5 +1,6 @@
 import sys
 import argparse
+from datetime import datetime
 
 import unittest
 from unittest.mock import patch
@@ -9,16 +10,21 @@ from ami_cleaner.libs import images as target
 
 class TestImages(unittest.TestCase):
 
+    image = {
+        'ImageId': 'ami-0123456789abc',
+        'Name': 'ami-name-0123456789abc',
+        'CreationDate': '2019-10-01T14:16:22.000Z'
+    }
+
+    def _create_parser(self, option, value):
+        sys.argv = ['']
+        arg_parser = argparse.ArgumentParser()
+        arg_parser.add_argument(option, default=value)
+        args = arg_parser.parse_args()
+        return args
+
     def test_print_tableview(self):
-        images = {
-            'Images': [
-                {
-                    'ImageId': 'ami-0123456789abc',
-                    'Name': 'ami-name-0123456789abc',
-                    'CreationDate': '2020-02-10T12:12:12.123456'
-                }
-            ]
-        }
+        images = {'Images': [self.image]}
         target.print_tableview(images)
 
     @patch('ami_cleaner.libs.images._get_images_should_deregister')
@@ -26,60 +32,141 @@ class TestImages(unittest.TestCase):
     def test_get_images_should_deregister(self,
                                           mocked_client,
                                           mocked_get_images_should_deregister):
-        sys.argv = ['']
-        arg_parser = argparse.ArgumentParser()
-        arg_parser.add_argument('--region', default='ap-northeast-1')
-        args = arg_parser.parse_args()
+        args = self._create_parser('--region', 'ap-northeast-1')
         target.get_images_should_deregister(args)
 
     @patch('ami_cleaner.libs.images._get_client')
     def test_deregister_image(self, mocked_client):
-        sys.argv = ['']
-        arg_parser = argparse.ArgumentParser()
-        arg_parser.add_argument('--region', default='ap-northeast-1')
-        args = arg_parser.parse_args()
+        args = self._create_parser('--region', 'ap-northeast-1')
         image_id = 'ami-0123456789abc'
         target.deregister_image(args, image_id)
 
     @patch('ami_cleaner.libs.images.boto3')
     def test_get_client(self, mocked_boto3):
-        sys.argv = ['']
-        arg_parser = argparse.ArgumentParser()
-        arg_parser.add_argument('--region', default='ap-northeast-1')
-        args = arg_parser.parse_args()
+        args = self._create_parser('--region', 'ap-northeast-1')
         target._get_client(args.region)
 
     @patch('ami_cleaner.libs.images._get_inatances_specific_image_id')
     @patch('ami_cleaner.libs.images._get_client')
     def test_is_using_image_true(self, mocked_client, mocked_get_inatances):
-        sys.argv = ['']
-        arg_parser = argparse.ArgumentParser()
-        arg_parser.add_argument('--region', default='ap-northeast-1')
-        args = arg_parser.parse_args()
-        image = {
-            'ImageId': 'ami-0123456789abc',
-            'Name': 'ami-name-0123456789abc',
-            'CreationDate': '2020-02-10T12:12:12.123456'
-        }
-
+        args = self._create_parser('--region', 'ap-northeast-1')
         mocked_get_inatances.return_value = {'Reservations': [{'Instances': ''}]}
-        response = target._is_using_image(args, image)
+        response = target._is_using_image(args, self.image)
         self.assertEqual(response, True)
 
     @patch('ami_cleaner.libs.images._get_inatances_specific_image_id')
     @patch('ami_cleaner.libs.images._get_client')
     def test_is_using_image_false(self, mocked_client, mocked_get_inatances):
-        sys.argv = ['']
-        arg_parser = argparse.ArgumentParser()
-        arg_parser.add_argument('--region', default='ap-northeast-1')
-        args = arg_parser.parse_args()
-        image = {
-            'ImageId': 'ami-0123456789abc',
-            'Name': 'ami-name-0123456789abc',
-            'CreationDate': '2020-02-10T12:12:12.123456'
-        }
-
+        args = self._create_parser('--region', 'ap-northeast-1')
         mocked_get_inatances.return_value = {'Reservations': []}
-        response = target._is_using_image(args, image)
+        response = target._is_using_image(args, self.image)
         self.assertEqual(response, False)
 
+    @patch('ami_cleaner.libs.images._get_datetime_now')
+    def test_is_creation_date_expire_limit_true(self, mocked_datetime_now):
+        args = self._create_parser('--days', 90)
+        datetime_now_str = "2020-04-01 12:00:00"
+        mocked_datetime_now.return_value = datetime.strptime(datetime_now_str, '%Y-%m-%d %H:%M:%S')
+        response = target._is_creation_date_expire_limit(args, self.image)
+        self.assertEqual(response, True)
+
+    @patch('ami_cleaner.libs.images._get_datetime_now')
+    def test_is_creation_date_expire_limit_false(self, mocked_datetime_now):
+        args = self._create_parser('--days', 90)
+        datetime_now_str = "2019-12-01 12:00:00"
+        mocked_datetime_now.return_value = datetime.strptime(datetime_now_str, '%Y-%m-%d %H:%M:%S')
+        response = target._is_creation_date_expire_limit(args, self.image)
+        self.assertEqual(response, False)
+
+    def test_get_datetime_now(self):
+        response = target._get_datetime_now()
+        self.assertEqual(isinstance(response, datetime), True)
+
+    def test__tag_filter_generate(self):
+        args = self._create_parser('--tag-filters', {'Name': 'test-image'})
+        target._tag_filter_generate(args)
+
+    # _get_images_should_deregister test case
+    #   1. no images in response of describe_images
+    #   2. images expire limit and using
+    #   3. images expire limit and not using
+    #   4. images not expire limit and using
+    #   5. images not expire limit and not using
+    @patch('ami_cleaner.libs.images._tag_filter_generate')
+    @patch('ami_cleaner.libs.images._get_client')
+    def test_get_images_should_deregister_no_images(self,
+                                                    mocked_client,
+                                                    mocked_tag_filter_generate):
+        args = self._create_parser('--owner', '0123456789')
+        mocked_client.describe_images.return_value = {'Images': []}
+        respons = target._get_images_should_deregister(mocked_client, args)
+        self.assertFalse(respons['Images'])
+
+    @patch('ami_cleaner.libs.images._is_using_image')
+    @patch('ami_cleaner.libs.images._is_creation_date_expire_limit')
+    @patch('ami_cleaner.libs.images._tag_filter_generate')
+    @patch('ami_cleaner.libs.images._get_client')
+    def test_get_images_should_deregister_is_limit_and_using(self,
+                                                             mocked_client,
+                                                             mocked_tag_filter_generate,
+                                                             mocked_is_expire_limit,
+                                                             mocked_is_using_image):
+        args = self._create_parser('--owner', '0123456789')
+        mocked_client.describe_images.return_value = {'Images': [self.image]}
+        mocked_is_expire_limit.return_value = True
+        mocked_is_using_image.return_value = True
+        respons = target._get_images_should_deregister(mocked_client, args)
+        self.assertFalse(respons['Images'])
+
+    @patch('ami_cleaner.libs.images._is_using_image')
+    @patch('ami_cleaner.libs.images._is_creation_date_expire_limit')
+    @patch('ami_cleaner.libs.images._tag_filter_generate')
+    @patch('ami_cleaner.libs.images._get_client')
+    def test_get_images_should_deregister_is_limit_and_not_using(self,
+                                                                 mocked_client,
+                                                                 mocked_tag_filter_generate,
+                                                                 mocked_is_expire_limit,
+                                                                 mocked_is_using_image):
+        args = self._create_parser('--owner', '0123456789')
+        mocked_client.describe_images.return_value = {'Images': [self.image]}
+        mocked_is_expire_limit.return_value = True
+        mocked_is_using_image.return_value = False
+        respons = target._get_images_should_deregister(mocked_client, args)
+        self.assertTrue(respons['Images'])
+
+    @patch('ami_cleaner.libs.images._is_using_image')
+    @patch('ami_cleaner.libs.images._is_creation_date_expire_limit')
+    @patch('ami_cleaner.libs.images._tag_filter_generate')
+    @patch('ami_cleaner.libs.images._get_client')
+    def test_get_images_should_deregister_is_not_limit_and_using(self,
+                                                                 mocked_client,
+                                                                 mocked_tag_filter_generate,
+                                                                 mocked_is_expire_limit,
+                                                                 mocked_is_using_image):
+        args = self._create_parser('--owner', '0123456789')
+        mocked_client.describe_images.return_value = {'Images': [self.image]}
+        mocked_is_expire_limit.return_value = False
+        mocked_is_using_image.return_value = True
+        respons = target._get_images_should_deregister(mocked_client, args)
+        self.assertFalse(respons['Images'])
+
+    @patch('ami_cleaner.libs.images._is_using_image')
+    @patch('ami_cleaner.libs.images._is_creation_date_expire_limit')
+    @patch('ami_cleaner.libs.images._tag_filter_generate')
+    @patch('ami_cleaner.libs.images._get_client')
+    def test_get_images_should_deregister_is_not_limit_and_not_using(self,
+                                                                     mocked_client,
+                                                                     mocked_tag_filter_generate,
+                                                                     mocked_is_expire_limit,
+                                                                     mocked_is_using_image):
+        args = self._create_parser('--owner', '0123456789')
+        mocked_client.describe_images.return_value = {'Images': [self.image]}
+        mocked_is_expire_limit.return_value = False
+        mocked_is_using_image.return_value = False
+        respons = target._get_images_should_deregister(mocked_client, args)
+        self.assertFalse(respons['Images'])
+
+    @patch('ami_cleaner.libs.images._get_client')
+    def test_get_inatances_specific_image_id(self, mocked_client):
+        mocked_client.describe_instances.return_value = {}
+        target._get_inatances_specific_image_id(mocked_client, self.image['ImageId'])
